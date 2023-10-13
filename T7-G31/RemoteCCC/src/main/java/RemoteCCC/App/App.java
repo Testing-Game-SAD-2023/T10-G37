0df3e93af85efe737c9471fdd58e16e141c2d490
SadProject1 <fedelegenny@gmail.com>
2023-10-13 12:42:57 +0200

    new update

--- T7-G31/RemoteCCC/src/main/java/RemoteCCC/App/App.java
+++ T7-G31/RemoteCCC/src/main/java/RemoteCCC/App/App.java
@@ -70,7 +70,7 @@ public class App {
         
 
         
-        if(compileExecuteCovarageWithMaven(output_maven)){
+        if(compileExecuteCovarageWithMaven(output_maven,request)){
             String retXmlJacoco = readFileToString(Config.getCoverageFolder());//zipSiteFolderToJSON(Config.getzipSiteFolderJSON()).toString();
             response.setError(false);
             response.setoutCompile(output_maven[0]);
@@ -128,7 +128,8 @@ public class App {
    
 
     //esegue compilazione con maven per ritornare eventuali errori utente
-    private static boolean compileExecuteCovarageWithMaven(String []ret) throws IOException, InterruptedException {
+    /*    VECCHIA VERSIOONE
+     private static boolean compileExecuteCovarageWithMaven(String []ret) throws IOException, InterruptedException {
 
         ProcessBuilder processBuilder = new ProcessBuilder();
 
@@ -156,7 +157,41 @@ public class App {
             return false;
         }
 
+    }*/
+private static boolean compileExecuteCovarageWithMaven(String []ret, RequestDTO request) throws IOException, InterruptedException {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String nome_test=request.getTestingClassName();
+        String nome_classe=request.getUnderTestClassName();
+        
+        processBuilder.command("evosuite", "-measureCoverage ", "-class", nome_classe ," Djunit=", nome_test," -Dcriterion=LINE");
+        processBuilder.directory(new File(Config.getpathCompiler()));
+    
+        Process process = processBuilder.start();
+        System.out.println("Ciao");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null)
+            ret[0] += line += "\n";
+        int exitCode = process.waitFor();
+       
+        // Legge il contenuto del buffer del terminale
+        // InputStream inputStream = process.getInputStream();
+        // byte[] buffer = new byte[inputStream.available()];
+        // inputStream.read(buffer);
+        // ret[0] = new String(buffer, StandardCharsets.UTF_8);
+        if (exitCode == 0) {
+            System.out.println("Maven clean compile executed successfully.");
+            return true;
+        } else {
+            System.out.println("Error executing Maven clean compile.");
+            return false;
+        }
+
     }
+
+
    
     /**
      * Metodo per salvare un file ".java"
