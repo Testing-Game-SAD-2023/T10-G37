e8802562ad6448650265b6eac90899c09cee5abc
SadProject1 <fedelegenny@gmail.com>
2023-10-26 11:59:10 +0200

    update java
    è stata modificata la chiamata ad evosuite mantenendo la versione 17 di java ed utilizzando quando serve la 1.8

--- T7-G31/RemoteCCC/ClientProject/codice.sh
+++ T7-G31/RemoteCCC/ClientProject/codice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$(pwd) inizio sh"
 mvn clean install
-java -jar evosuite-1.0.6.jar -measureCoverage -class ClientProject.$1 -Djunit=ClientProject.$2 -projectCP /ClientProject/target/classes:/ClientProject/target/test-classes -Dcriterion=LINE
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar evosuite-1.0.6.jar -measureCoverage -class ClientProject.$1 -Djunit=ClientProject.$2 -projectCP /ClientProject/target/classes:/ClientProject/target/test-classes -Dcriterion=LINE
--- T7-G31/RemoteCCC/entrypoint.sh
+++ T7-G31/RemoteCCC/entrypoint.sh
@@ -5,9 +5,9 @@ apt-get -y update
 apt-get -y install maven git unzip
 apt-get -y install wget
 echo "le versioni di java e javac sono le seguenti, è necessaria la versione 1.8: "
-update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac 1
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javac
+#update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac 1
+#update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+#update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javac
 java -version
 javac -version
 wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
